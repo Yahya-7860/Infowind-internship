607e00aaefc7f6e2897985f0072afacb504e9175
Yahya Ansari <148518803+Yahya-7860@users.noreply.github.com>
2024-07-31 16:18:16 +0530

    Add files via upload
    webpack implemented, now without live server, the site will run on file format with all modules.

---
+++ TODO_Basic/assests/index.js
@@ -0,0 +1,23 @@
+let btn = document.querySelector(".addBtn");
+let ul = document.querySelector(".task");
+let text = document.querySelector(".textValue");
+// console.log('javascript runs through a single file in browser')
+
+import { strikeThr, addTask, removeTask } from "./moduleBundle.js";
+
+//pressing 'add' button to add list
+btn.addEventListener("click",addTask)
+
+//pressing enter key to add list
+text.addEventListener('keypress',(e)=>{
+    if(e.key=="Enter"){
+        e.preventDefault();
+        addTask();
+    }
+})
+
+//marking the task (strike through)
+ul.addEventListener("click",strikeThr)
+
+//removing added list
+ul.addEventListener("click",removeTask)
